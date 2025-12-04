1551654e689f19d4dcb445fec524ba3acbf0adbc
accursoj <jfaccurso@gmail.com>
2025-12-03 21:48:15 -0600

    Test success: deep sleep, wakeup,  and rgb led indicators

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <nvs.h>
 #include "driver/gpio.h"
+#include "driver/rtc_io.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_sleep.h"
@@ -12,6 +13,7 @@
 
 #define RGB_LED_PIN 38
 #define POWER_PIN GPIO_NUM_3
+#define LED_BRIGHTNESS 25
 
 void init_gpio() {
     gpio_config_t io_conf = {};
@@ -31,7 +33,7 @@ void init_gpio() {
     io_conf.pull_up_en = GPIO_PULLUP_DISABLE;
     io_conf.pull_down_en = 0;
     io_conf.intr_type = GPIO_INTR_DISABLE;
-    gpio_config(&io_conf);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
 
     // Configure input pins
     io_conf.pin_bit_mask = ((1ULL << LCD_SDI_PIN) |
@@ -41,18 +43,25 @@ void init_gpio() {
     io_conf.mode = GPIO_MODE_INPUT;
     io_conf.pull_up_en = 1;
     io_conf.intr_type = GPIO_INTR_POSEDGE;
-    gpio_config(&io_conf);
+    ESP_ERROR_CHECK(gpio_config(&io_conf));
+
+    ESP_ERROR_CHECK(rtc_gpio_init(POWER_PIN));
+    ESP_ERROR_CHECK(rtc_gpio_set_direction(POWER_PIN, RTC_GPIO_MODE_INPUT_ONLY));
+    // Ensure pin will not droop low
+    ESP_ERROR_CHECK(rtc_gpio_pullup_en(POWER_PIN));
+    ESP_ERROR_CHECK(rtc_gpio_pulldown_dis(POWER_PIN));
 }
 
 void start_deep_sleep() {
     if (esp_sleep_is_valid_wakeup_gpio(POWER_PIN))      // check if GPIO3 can enable ext0 wakeup
     {
-        esp_sleep_enable_ext0_wakeup(POWER_PIN, 1);
+        esp_sleep_enable_ext0_wakeup(POWER_PIN, 0);
         esp_deep_sleep_start();
     }
 }
-static led_strip_handle_t board_led_handle = NULL;
-void start_rgb_indicator(void) {
+
+led_strip_handle_t init_rgb_indicator(void) {
+    led_strip_handle_t board_led_handle = NULL;
     
     led_strip_config_t strip_config = {};
     strip_config.strip_gpio_num = RGB_LED_PIN;
@@ -66,46 +75,38 @@ void start_rgb_indicator(void) {
 
     ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &strip_rmt_config, &board_led_handle));
 
-    ESP_ERROR_CHECK(led_strip_set_pixel(board_led_handle, 0, 255, 255, 0));
+    return board_led_handle;
+}
 
+void show_rgb_led(led_strip_handle_t board_led_handle, uint32_t color_r, uint32_t color_g, uint32_t color_b, uint32_t brightness) {
+    ESP_ERROR_CHECK(led_strip_set_pixel(
+        board_led_handle,
+        (0*brightness)/255,
+        (color_r*brightness)/255,
+        (color_g*brightness)/255,
+        (color_b*brightness)/255)
+    );
     ESP_ERROR_CHECK(led_strip_refresh(board_led_handle));
-    // ledc_timer_config_t ledc_timer = {};
-    // ledc_timer.speed_mode = LEDC_LOW_SPEED_MODE;
-    // ledc_timer.timer_num = LEDC_TIMER_0;
-    // ledc_timer.duty_resolution = LEDC_TIMER_13_BIT;
-    // ledc_timer.freq_hz = 4000;
-    // ledc_timer.clk_cfg = LEDC_AUTO_CLK;
-
-    // ESP_ERROR_CHECK(ledc_timer_config(&ledc_timer));
-
-    // ledc_channel_config_t ledc_channel = {};
-    // ledc_channel.gpio_num = RGB_LED_PIN;
-    // ledc_channel.speed_mode = LEDC_LOW_SPEED_MODE;
-    // ledc_channel.channel = LEDC_CHANNEL_0;
-    // ledc_channel.intr_type = LEDC_INTR_DISABLE;
-    // ledc_channel.timer_sel = LEDC_TIMER_0;
-    // ledc_channel.duty = 0;
-    // ledc_channel.hpoint = 0;
-
-    // ESP_ERROR_CHECK(ledc_channel_config(&ledc_channel));
-
-    // ESP_ERROR_CHECK(ledc_set_duty(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_0, 4096));
 }
 
 void system_boot() {
     init_gpio();
-    start_rgb_indicator();
-    vTaskDelay(1000 / portTICK_PERIOD_MS);      // pause for one second
-    start_deep_sleep();
+    led_strip_handle_t board_led_handle = init_rgb_indicator();
+    show_rgb_led(board_led_handle, 255, 255, 0, LED_BRIGHTNESS);        // yellow
+    vTaskDelay(1000 / portTICK_PERIOD_MS);              // pause for one second
+
+    esp_sleep_wakeup_cause_t deep_wakeup_cause = esp_sleep_get_wakeup_cause();
+    if (deep_wakeup_cause != ESP_SLEEP_WAKEUP_EXT0) {
+        start_deep_sleep();
+    }
+    show_rgb_led(board_led_handle, 0, 255, 0,LED_BRIGHTNESS);
     init_ecg();
     init_lcd();
+    // show_boot_screen_no_dma();
+    // show_boot_screen();
     show_boot_screen_lvgl();
 }
 
-void show_green_board_led() {
-    ESP_ERROR_CHECK(led_strip_set_pixel(board_led_handle, 0, 0, 255, 0));
-    ESP_ERROR_CHECK(led_strip_refresh(board_led_handle));
-}
 
 void power_down() {
     ecg_power_down();
@@ -113,12 +114,9 @@ void power_down() {
 
 void app_main() {
     system_boot();
-    show_boot_screen();
-    show_boot_screen_lvgl();
     // TODO: Check ECG alarms
     stream_ecg_data();
-    show_green_board_led();
-    while (1){}
 
+    while (1){}
     power_down();
 }
